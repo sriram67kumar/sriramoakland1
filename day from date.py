195afd6789e476b861c9baed10fb06b4e9f745f2
sriram67kumar <146435814+sriram67kumar@users.noreply.github.com>
2023-09-30 11:28:50 +0530

    jii
    import datetime
    
    # Input date in the format 'YYYY-MM-DD'
    date_str = input("Enter a date (YYYY-MM-DD): ")
    
    try:
        # Parse the input date string into a datetime object
        date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
        
        # Get the day of the week as an integer (0 = Monday, 6 = Sunday)
        day_of_week = date_obj.weekday()
        
        # Define a list of day names
        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
        
        # Print the day of the week
        print(f"The day of the week for {date_str} is {days[day_of_week]}.")
    except ValueError:
        print("Invalid date format. Please use YYYY-MM-DD format.")

---
+++ day from date.py
@@ -0,0 +1,19 @@
+import datetime
+
+# Input date in the format 'YYYY-MM-DD'
+date_str = input("Enter a date (YYYY-MM-DD): ")
+
+try:
+    # Parse the input date string into a datetime object
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    
+    # Get the day of the week as an integer (0 = Monday, 6 = Sunday)
+    day_of_week = date_obj.weekday()
+    
+    # Define a list of day names
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    # Print the day of the week
+    print(f"The day of the week for {date_str} is {days[day_of_week]}.")
+except ValueError:
+    print("Invalid date format. Please use YYYY-MM-DD format.")
